a9d339ba104ef0c5c985ade335cc6bd71664545e
Beatrice <beatrice.calb12@gmail.com>
2026-05-14 13:41:58 +0300

    Modificari PanTilt setari

--- src/runtime_config.py
+++ src/runtime_config.py
@@ -46,24 +46,24 @@ class UndistortConfig:
 
 @dataclass(frozen=True)
 class PanTiltConfig:
-    enabled: bool = False  
-    backend: str = "mock"
+    enabled: bool = True 
+    backend: str = "arducam"
     pan_channel: int = 0 #canalul pt pan
     tilt_channel: int = 1 #canalul pt tilt
-    invert_pan: bool = False
-    invert_tilt: bool = False
-    update_every_n_frames: int = 5
-    dead_zone_x: int = 90
-    dead_zone_y: int = 70
-    error_smoothing_alpha: float = 0.25 #procentaj pt eroarea curenta (1 este fara smoothing)
-    min_move_updates: int = 2 #la cate update-uri in afara dead zone se ia in considerare miscarea 
+    invert_pan: bool = False    
+    invert_tilt: bool = False   
+    update_every_n_frames: int = 2
+    dead_zone_x: int = 50
+    dead_zone_y: int = 40
+    error_smoothing_alpha: float = 0.35#procentaj pt eroarea curenta (1 este fara smoothing)
+    min_move_updates: int = 1 #la cate update-uri in afara dead zone se ia in considerare miscarea 
     pan_start: float = 90.0
     tilt_start: float = 90.0
     pan_min: float = 0.0
     pan_max: float = 180.0
     tilt_min: float = 0.0
     tilt_max: float = 180.0
-    step_degrees: float = 1.0
+    step_degrees: float = 3.0
 
 
 @dataclass(frozen=True)
